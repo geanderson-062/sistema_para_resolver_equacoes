ad316518fb89e48c130f57528f7dc5c061b018cc
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-04 01:05:46 -0300

    sistema funcional

---
+++ main.py
@@ -0,0 +1,69 @@
+#importando a lib
+import customtkinter
+from tkinter import * 
+import numpy as np
+import matplotlib.pyplot as plt
+
+#alterando a tematica do sistema
+customtkinter.set_appearance_mode ("dark")
+customtkinter.set_default_color_theme("green") # outro thema dark-blue
+
+#criando a janela
+janela= customtkinter.CTk()
+
+#definindo tamanho da janela
+janela.geometry("700x400")
+
+#definindo o titulo do sistema
+janela.title("Sistema de Equações de segundo grau")
+
+#trocando o icone do sistema
+janela.iconbitmap("icone.ico")
+
+#impedindo reajuste da janela evitando que o usuario quebre a janela 
+janela.resizable(False, False)
+
+#definindo uma ação para o botão executar 
+def clique():
+   
+    Equacao1 = equacao1.get()
+    Equacao2 = equacao2.get()
+
+    # Gerar valores para o eixo x
+    x = np.linspace(-10, 10, 100)
+
+    # Calcular os valores correspondentes no eixo y para a primeira equação
+    y1 = eval(Equacao1)
+
+    # Calcular os valores correspondentes no eixo y para a segunda equação
+    y2 = eval(Equacao2)
+
+    # Plotar o gráfico das equações
+    plt.plot(x, y1, label="X")
+    plt.plot(x, y2, label="Y")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Gráfico das Equações")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+# escrevendo na janela 
+texto = customtkinter.CTkLabel(janela, text="Bem vindo", font=("Arial", 30))
+
+#colocando texto na janela e passando a distancia dos itens 
+texto.pack(padx=10, pady=40)
+
+#criando campo para captar os dados
+equacao1 = customtkinter.CTkEntry(janela, placeholder_text="Digite a primeira equação",width=(200))
+equacao1.pack(padx=10, pady=10)
+
+equacao2 = customtkinter.CTkEntry(janela, placeholder_text="Digite a segunda equação", width=(200))
+equacao2.pack(padx=10, pady=10)
+
+#colocando um botão 
+botao = customtkinter.CTkButton(janela, text="Resolver", command=clique, width=(200))
+botao.pack(padx=10, pady=10)
+
+#rodando a janela
+janela.mainloop()
