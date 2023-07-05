f6a093493c64caccddb54df043f8e696cf27d500
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-05 11:32:45 -0300

    validação dos dados digitados

--- main.py
+++ main.py
@@ -1,70 +1,94 @@
-#importando as libs
+# Importando as libs
 import customtkinter
-from tkinter import * 
+from tkinter import *
 import numpy as np
 import matplotlib.pyplot as plt
+from tkinter import messagebox
 
-#alterando a tematica do sistema
-customtkinter.set_appearance_mode ("dark")
-customtkinter.set_default_color_theme("green") # outro thema dark-blue
+# Alterando a temática do sistema
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")  # Outro tema dark-blue
 
-#criando a janela
-janela= customtkinter.CTk()
+# Criando a janela
+janela = customtkinter.CTk()
 
-#definindo tamanho da janela
+# Definindo tamanho da janela
 janela.geometry("700x400")
 
-#definindo o titulo do sistema
+# Definindo o título do sistema
 janela.title("Sistema de Equações")
 
-#trocando o icone do sistema
+# Trocando o ícone do sistema
 janela.iconbitmap("./assets/icone.ico")
 
-#impedindo reajuste da janela evitando que o usuario quebre a janela 
+# Impedindo reajuste da janela evitando que o usuário quebre a janela
 janela.resizable(False, False)
 
-#definindo uma ação para o botão executar 
+# Definindo uma ação para o botão executar
 def clique():
-   
-    #pegando os dados digitados e colocando na vareavel Equacao1 e 2 
+    # Pegando os dados digitados e colocando na variável Equacao1 e 2
     Equacao1 = equacao1.get()
     Equacao2 = equacao2.get()
 
-    # Gerar valores para o eixo x
-    x = np.linspace(-10, 10, 100)
+    #padrão aceitado pelo sistema numeros e simbolos
+    padrao = r'^[0-9+*/\-()\s]+$'
 
-    # Calcular os valores correspondentes no eixo x para a primeira equação
-    y1 = eval(Equacao1)
+    #se o que for digitado não for numero nem simbolo seja digitado no sistema faça isso
+    if (Equacao1 != padrao) and (Equacao2 != padrao):
+        messagebox.showerror("Erro", "Digite apenas numeros e simbolos, Por favor, tente novamente.")
 
-    # Calcular os valores correspondentes no eixo y para a segunda equação
-    y2 = eval(Equacao2)
+    #caso nada seja digitado no sistema faça isso
+    if (Equacao1 == "") and (Equacao2 == ""):
+        messagebox.showerror("Erro", "Nenhuma equação foi digitada ou ocorreu um erro. Por favor, tente novamente.")
 
-    # Plotar o gráfico das equações
-    plt.plot(x, y1, label="X")
-    plt.plot(x, y2, label="Y")
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title("Gráfico das Equações")
-    plt.grid(True)
-    plt.legend()
-    plt.show()
+    #caso nada seja digitado no campo equação X faça isso
+    if (Equacao1 == "") :  
+        messagebox.showerror("Erro", "a equação X NÂO foi digitada .")
 
-# escrevendo na janela 
+    #caso nada seja digitado no campo equação Y faça isso
+    if (Equacao2 == ""):
+        messagebox.showerror("Erro", "a equação Y NÂO foi digitada .")
+   
+    #se tudo tiver certo execute o calculo e o grafico
+    else:
+         # Gerar valores para o eixo x
+        x = np.linspace(-10, 10, 100)
+
+        # Calcular os valores correspondentes no eixo x para a primeira equação
+        y1 = eval(Equacao1)
+
+        # Calcular os valores correspondentes no eixo y para a segunda equação
+        y2 = eval(Equacao2)
+
+        # Plotar o gráfico das equações
+        plt.plot(x, y1, label="X")
+        plt.plot(x, y2, label="Y")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.title("Gráfico das Equações")
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+
+# Escrevendo na janela
 texto = customtkinter.CTkLabel(janela, text="Bem vindo", font=("Arial", 30))
 
-#colocando texto na janela e passando a distancia dos itens 
+# Colocando texto na janela e passando a distância dos itens
 texto.pack(padx=10, pady=40)
 
-#criando campo para captar os dados
-equacao1 = customtkinter.CTkEntry(janela, placeholder_text="Digite a equação X",width=(200))
+# Criando campo para captar os dados
+
+#campo equação X
+equacao1 = customtkinter.CTkEntry(janela, placeholder_text="Digite a equação X", width=(200))
 equacao1.pack(padx=10, pady=10)
 
+#campo equação Y
 equacao2 = customtkinter.CTkEntry(janela, placeholder_text="Digite a equação Y", width=(200))
 equacao2.pack(padx=10, pady=10)
 
-#colocando um botão 
+# Colocando um botão para enviar os dados
 botao = customtkinter.CTkButton(janela, text="Resolver", command=clique, width=(200))
 botao.pack(padx=10, pady=10)
 
-#rodando a janela
+# Rodando a janela
 janela.mainloop()
